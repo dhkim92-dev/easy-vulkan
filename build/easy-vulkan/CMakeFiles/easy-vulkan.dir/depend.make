f84aa40e3ef16d8c15768677d2ba14d46946861b
dhkim92 <dhkim92.dev@gmail.com>
2025-06-26 00:20:14 +0900

    빌드/아웃풋 디렉터리 제거

--- build/easy-vulkan/CMakeFiles/easy-vulkan.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for easy-vulkan.
-# This may be replaced when dependencies are built.
--- output/include/easy-vulkan.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ev-utility.h"
-#include "ev-instance.h"
-#include "ev-logger.h"
-#include "ev-pdevice.h"
-#include "ev-device.h"
-#include "ev-swapchain.h"
-#include "initializer/ev-initializer.h"
