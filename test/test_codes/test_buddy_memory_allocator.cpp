498e1d236010ba82eb4f34d37159bf71ae684ebb
dohoon <elensar92@gmail.com>
2025-07-07 23:40:40 +0900

    Linux 테스트 완료

--- test/test_codes/test_buddy_memory_allocator.cpp
+++ test/test_codes/test_buddy_memory_allocator.cpp
@@ -28,3 +28,17 @@ TEST_F(BuddyMemoryAllocatorTest, AddAndBuildMemoryPool) {
     VkResult result = allocator->build();
     EXPECT_EQ(result, VK_SUCCESS);
 }
+
+TEST_F(BuddyMemoryAllocatorTest, AllocateAndFreeBuffer) {
+    auto allocator = std::make_shared<ev::BitmapBuddyMemoryAllocator>(device);
+    ASSERT_NE(allocator, nullptr);
+    allocator->add_pool(ev::memory_type::GPU_ONLY, 1024 * 1024); // 1MB
+    VkResult result = allocator->build();
+    EXPECT_EQ(result, VK_SUCCESS);
+
+    auto buffer = std::make_shared<ev::Buffer>(device, 256 * 1024, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+    result = allocator->allocate_buffer(buffer, ev::memory_type::GPU_ONLY);
+    EXPECT_EQ(result, VK_SUCCESS);
+
+    buffer.reset();
+}
