aba3f9fee0a664e2a08562ee654dbc725641162b
dohoon <elensar92@gmail.com>
2025-07-22 19:24:00 +0900

    install.sh 제거

--- install.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-#rm -rf build output
-
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF
-cmake --build build --config Release
-cmake --install build --prefix ./output --config Release
